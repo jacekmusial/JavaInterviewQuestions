32fa743f96de2f76c617f4d2ff1674ccce3647c6
Jacek Musiał <jacek@musi.al>
2015-11-10 00:06:54 +0100

    added xml parser file

---
+++ app/src/main/java/al/musi/javainterviewquestions/XmlQuestionsParser.java
@@ -0,0 +1,146 @@
+package al.musi.javainterviewquestions;
+
+import android.util.Log;
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by re on 2015-11-07.
+ */
+public class XmlQuestionsParser  {
+    /* We don't use namespaces */
+    private static final String ns = null;
+
+    public List<Entry> parse(InputStream in) throws XmlPullParserException, IOException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+            parser.nextTag();
+            return readFeed(parser);
+        } finally {
+            in.close();
+        }
+    }
+
+    private List<Entry> readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List<Entry> entries = new ArrayList<Entry>();
+
+        parser.require(XmlPullParser.START_TAG, ns, "topjava");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            // Starts by looking for the entry tag
+            if (name.equals("entry")) {
+                Log.d("xmlparser:", "+ENTRY ");
+                entries.add(readEntry(parser));
+            } else {
+                skip(parser);
+            }
+        }
+        return entries;
+    }
+
+    // This class represents a single entry (post) in the XML.
+    // It includes the data like "id", "question" and "answer".
+    public static class Entry {
+        public final String id;
+        public final String question;
+        public final String answer;
+
+        public Entry( String id, String question, String answer) {
+            this.id = id;
+            this.question = question;
+            this.answer = answer;
+        }
+    }
+
+    // Parses the contents of an entry. If it encounters a id, question, or
+    // answer tag, hands them off to their respective "read" methods for processing.
+    // Otherwise, skips the tag.
+    private Entry readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "entry");
+        String id = null;
+        String question = null;
+        String answer = null;
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("id")) {
+                id = readId(parser);
+            } else if (name.equals("question")) {
+                question = readQuestion(parser);
+            } else if (name.equals("answer")) {
+                answer = readAnswer(parser);
+            } else {
+                skip(parser);
+            }
+        }
+        return new Entry(id, question, answer);
+    }
+
+    // Processes id tags in the feed.
+    private String readId(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "id");
+        String id = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "id");
+        return id;
+    }
+
+    // Processes question tags in the feed.
+    private String readQuestion(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "question");
+        String question = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "question");
+        return question;
+    }
+
+    // Processes answer tags in the feed.
+    private String readAnswer(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "answer");
+        String answer = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "answer");
+        return answer;
+    }
+
+    // For the tags title and summary, extracts their text values.
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+
+    // Skips tags the parser isn't interested in. Uses depth to handle nested tags. i.e.,
+    // if the next tag after a START_TAG isn't a matching END_TAG, it keeps going until it
+    // finds the matching END_TAG (as indicated by the value of "depth" being 0).
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
+}
